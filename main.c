3fc363a492164399e3cc2330cf30d7a5af2e6c1d
dradkowski <cyberdogpolska@gmail.com>
2018-10-12 17:37:55 +0200

    ładne

--- main.c
+++ main.c
@@ -3,30 +3,35 @@
 
 int main() {
 
-    int a, b, c;
-    double delta, x1, x2;
+    double a, b, c, delta, x1, x2;
 
-    printf("Podaj a ");
-    scanf(" %i", &a);
+    printf("probram oblicza pierwiastki równania kwadratowego\n");
+    printf("podaj wsółczynniki \n");
 
-    printf("Podaj b ");
-    scanf(" %i", &b);
+    printf("Podaj a "); scanf(" %lf", &a);
+    printf("Podaj b "); scanf(" %lf", &b);
+    printf("Podaj c "); scanf(" %lf", &c);
 
-    printf("Podaj c ");
-    scanf(" %i", &c);
+    if (!a) {
+        printf("to nie jest równanie kwadratowe");
+        return 0;
+    }
 
     delta = b * b - 4 * a * c;
 
-    if (delta > 0) {
+    if (delta) {
         x1 = (b * (-1) - sqrt(delta)) / (2 * a);
         x2 = (b * (-1) + sqrt(delta)) / (2 * a);
         printf("pierwiasteki to %lf %lf", x1, x2);
-    }else if( delta == 0){
-            x1 = (b * (-1) ) / (2 * a);
-            printf("pierwiastek to %lf", x1);
-    } else{
-        printf("brak pierwiastków");
+        return 0;
     }
-    
+    if (!delta) {
+        x1 = (b * (-1)) / (2 * a);
+        printf("pierwiastek to %lf", x1);
+        return 0;
+    }
+
+    printf("brak pierwiastków");
+
     return 0;
 }
