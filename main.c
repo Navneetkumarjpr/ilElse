8fdabb3d894f4c362886d246ec4298f97bfe4409
dradkowski <cyberdogpolska@gmail.com>
2018-10-12 18:07:15 +0200

    dziala

--- main.c
+++ main.c
@@ -1,37 +1,54 @@
 #include <stdio.h>
 #include <math.h>
 
+void wczytajParametry(double *a, double *b, double *c);
+void wyswietlInfo();
+double obliczDelte(double a, double b, double c);
+
 int main() {
 
     double a, b, c, delta, x1, x2;
 
-    printf("probram oblicza pierwiastki równania kwadratowego\n");
-    printf("podaj wsółczynniki \n");
-
-    printf("Podaj a "); scanf(" %lf", &a);
-    printf("Podaj b "); scanf(" %lf", &b);
-    printf("Podaj c "); scanf(" %lf", &c);
+    wyswietlInfo();
+    wczytajParametry(&a, &b, &c);
+    delta = obliczDelte(a, b, c);
 
     if (!a) {
         printf("to nie jest równanie kwadratowe");
         return 0;
     }
 
-    delta = b * b - 4 * a * c;
-
     if (delta) {
         x1 = (b * (-1) - sqrt(delta)) / (2 * a);
         x2 = (b * (-1) + sqrt(delta)) / (2 * a);
-        printf("pierwiasteki to %lf %lf", x1, x2);
+        printf("Równanie posiada dwa pierwiaski to: \nx1 = %lf \nx2 = %lf", x1, x2);
         return 0;
     }
+
     if (!delta) {
         x1 = (b * (-1)) / (2 * a);
-        printf("pierwiastek to %lf", x1);
+        printf("Istnieje tylko jeden pierwiastek i wynosi %lf", x1);
         return 0;
     }
 
     printf("brak pierwiastków");
 
     return 0;
+}
+
+double obliczDelte(double a, double b, double c) {
+    double delta;
+    delta = b * b - 4 * a * c;
+    return delta;
+}
+
+void wyswietlInfo() {
+    printf("probram oblicza pierwiastki równania kwadratowego\n");
+    printf("podaj wsółczynniki \n");
+}
+
+void wczytajParametry(double *a, double *b, double *c) {
+    printf("Podaj a "); scanf(" %lf", a);
+    printf("Podaj b "); scanf(" %lf", b);
+    printf("Podaj c "); scanf(" %lf", c);
 }
